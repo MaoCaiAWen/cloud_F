be659659187e6b82aa0edfdd2a4b1927bfd83428
王文博 <12345678>
2019-12-13 11:42:37 +0800

    添加cloud Sleuth链路追踪zipkin方式 6002、7001、7002添加zipkin gateway网关集成zipkin

--- consumer6002/src/main/java/com/example/consumer6002/Consumer6002Application.java
+++ consumer6002/src/main/java/com/example/consumer6002/Consumer6002Application.java
@@ -2,10 +2,14 @@ package com.example.consumer6002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class Consumer6002Application {
 
     public static void main(String[] args) {
